f0145f0ff587936106fc83a726ee2f48f8ee97e6
조원후 <purplehaze0@naver.com>
2025-07-23 16:48:10 +0900

    [Feat] (Network) PhotonManager 의 씬전환

--- Assets/JWH/JWH_Scrips/PhotonManager.cs
+++ Assets/JWH/JWH_Scrips/PhotonManager.cs
@@ -1,23 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
-using Photon.Chat;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
-using UnityEngine;
 
 public class PhotonManager : MonoBehaviourPunCallbacks
 {
-    private void Start()
+    public static PhotonManager Instance;
+
+    private string testloginScene = "JWH_LoginScene";
+    private string testlobbyScene = "JWH_LobbyScene";
+    private string testgameScene = "JWH_GameScene";
+
+    void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+    
+    public void ConnectToServer()
+    {
+        PhotonNetwork.NickName = "Player";
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
     }
-    public override void OnDisconnected(DisconnectCause cause)
+
+    public override void OnJoinedLobby()
     {
-        base.OnDisconnected(cause);
+        Debug.Log("로비");
+        SceneManager.LoadScene(testlobbyScene);
+    }
+
+   
+    public void CreateOrJoinRoom()
+    {
+        string roomName = "TestRoom";
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 입장");
+
+        
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            if (PhotonNetwork.IsMasterClient)
+                PhotonNetwork.LoadLevel(testgameScene);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2 && PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(testgameScene);
+        }
+    }
+
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(testlobbyScene);
     }
 }
