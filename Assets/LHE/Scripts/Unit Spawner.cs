c87ef6b9cecf4b0ca01252c041341ebab3586e91
Usanso <save1086@naver.com>
2025-07-23 17:43:01 +0900

    [Feat] 카메라 컨트롤러 및 스포너 구상
    - 화면 좌우에 마우스를 가까이 대면 화면이 이동함
    - 마우스 가두기
    - 약간의 가속도 있음
    
    - 스포너 스크립트 구상 필요

---
+++ Assets/LHE/Scripts/Unit Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LHE
+{
+    public class UnitSpawner : MonoBehaviour
+    {
+        // À¯´Ö ÇÁ¸®ÆÕ
+
+        // ½ºÆù À§Ä¡ p1,p2
+
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
