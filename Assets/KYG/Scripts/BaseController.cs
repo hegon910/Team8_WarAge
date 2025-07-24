5147d7b3e1a325de1467bf5ada0afbe3c20e846b
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-07-24 10:54:39 +0900

    [feat] Base HP
    기지 체력 관련 기능 구현
    
    네트워크 사용을 위한
    using Photon.Pun; 추가
    
    PhotonView사용을 위해
    MonoBehaviourPunCallbacks 변경
    
    프로퍼티로 체력 외부 접근 캡슐화
    
    게임 시작시 기지 초기화 진행하여
    기지 체력 UI와 연동 가능하게 코드 추가

--- Assets/KYG/Scripts/BaseController.cs
+++ Assets/KYG/Scripts/BaseController.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun; // 네트워크 연동을 위한 using
 
-public class BaseController : MonoBehaviour
+namespace KYG
+{
+    
+public class BaseController : MonoBehaviourPunCallbacks // PUN 연동 시 PhotonView 사용 가능
 {
     /// <summary>
     /// 기지 시스템 컨트롤러
@@ -11,15 +15,38 @@ public class BaseController : MonoBehaviour
     /// 네트워크 담당자와 초기 스텟 존재 유무 논의 필요 초기 스텟만 받아오면 될지도
     /// 네트워크와 동기화 로직 필요
     /// </summary>
-    [SerializeField] public int maxHP; // 최대 체력
+    [SerializeField] private int maxHP; // 최대 체력
 
-    [SerializeField] public int currentHP; // 현재 체력
+    private int currentHP; // 임의로 수정 불가능한 현재 체력
+
+    // 프로퍼티로 외부 접근 캡슐화
+    public int MaxHP => maxHP; 
+    
+    public int CurrentHP => currentHP; 
+    
+    
 
     [Header("Spawner")] public Transform spawnerPoint; // 유닛 생성 위치
 
+    public event Action<int, int> OnHpChanged; //HP 변동시 이벤트 발생 (최대 체력, 현재 체력) 
+
+    private PhotonView pv; 
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>(); // PhotonView 함수 초기화
+    }
+
     public void Start() // 게임 시작시 
     {
+        InitBase(); // 기지 초기화
+    }
+
+    public void InitBase()
+    {
         currentHP = maxHP; // 현재 체력 = 최대 체력으로 초기화
+        OnHpChanged?.Invoke(currentHP, maxHP); // 이벤트 발생
+        //InGameUIManager.Instance?.UpdateBaseHpUI(currentHP, maxHP); // UI연동
     }
     
     /// <summary>
@@ -28,7 +55,7 @@ public class BaseController : MonoBehaviour
     /// 데미지를 받아 현재 체력이 0이 되면 게임 매니저에 게임 오버 연동
     /// 체력이 0이 될시 파괴되는 에니메이션은 추가 과제
     /// </summary>
-    public void TakeDamageBass()
+    public void TakeDamageBase()
     {
         
     }
@@ -47,4 +74,5 @@ public class BaseController : MonoBehaviour
     
     // TODO 터렛 설치
     
+}
 }
