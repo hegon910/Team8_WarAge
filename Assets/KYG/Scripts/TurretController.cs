a99a9c94829f5bc9fe5c44b00c90bd943ed00558
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-07-28 12:02:28 +0900

    [feat] TurretData
    '터렛' 기능 구현 중
    
    터렛의 주요 정보를
    스크립터블 오브젝트로 구현
    편리하게 관리하고 수정,추가 가 쉽다
    
    게임 테스트시에 게임 벨런스를 잡기 수월하다

---
+++ Assets/KYG/Scripts/TurretController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace KYG
+{
+    
+    public class TurretController : MonoBehaviourPunCallbacks, IPunObservable
+    {
+    
+    }
+}
