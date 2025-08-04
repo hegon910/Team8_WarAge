18b370436effb550879c3377a8ca77f1e3a083a0
KIMYOUNGGEUN <doctor6749@gmail.com>
2025-08-04 15:16:26 +0900

    [chore] UltimateSkill
    '궁극기' 구현
    
    궁극기 전체 관리할
    UltimateSkillManager
    스크립트 생성
    
    AgeData에 궁극기 데이터 추가
    
    궁극기 탄환 이미지 추가

---
+++ Assets/KYG/Scripts/UltimateSkillManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace KYG
+{
+    /// <summary>
+    /// 궁극기 발동 및 쿨타임 관리, Photon 연동
+    /// </summary>
+    public class UltimateSkillManager : MonoBehaviourPun
+    {
+        
+    }
+}
