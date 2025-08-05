2e3b5f47f9979ef013cd7492946bbcb075cf54c1
조원후 <purplehaze0@naver.com>
2025-08-05 11:03:22 +0900

    [Docs] 안쓰는 기능 주석

--- Assets/JWH/JWH_Scrips/JWH_FIRE/UserAuthService_Final.cs
+++ Assets/JWH/JWH_Scrips/JWH_FIRE/UserAuthService_Final.cs
@@ -152,41 +152,41 @@ public class UserAuthService : MonoBehaviour
     }
 
 
-    public void ResetPassword(string email, Action<bool> callback = null)
-    {
-        Auth.SendPasswordResetEmailAsync(email)
-            .ContinueWithOnMainThread(task =>
-            {
-                if (task.IsCanceled || task.IsFaulted)
-                {
-                    Debug.LogError("패스워드 재설정 실패: " + task.Exception);
-                    callback?.Invoke(false);
-                }
-                else
-                {
-                    Debug.Log("패스워드 재설정 메일 전송 성공");
-                    callback?.Invoke(true);
-                }
-            });
-    }
+    //public void ResetPassword(string email, Action<bool> callback = null)
+    //{
+    //    Auth.SendPasswordResetEmailAsync(email)
+    //        .ContinueWithOnMainThread(task =>
+    //        {
+    //            if (task.IsCanceled || task.IsFaulted)
+    //            {
+    //                Debug.LogError("패스워드 재설정 실패: " + task.Exception);
+    //                callback?.Invoke(false);
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("패스워드 재설정 메일 전송 성공");
+    //                callback?.Invoke(true);
+    //            }
+    //        });
+    //}
 
-    public void DeleteAccount(Action<bool> callback = null)
-    {
-        Auth.CurrentUser.DeleteAsync()
-            .ContinueWithOnMainThread(task =>
-            {
-                if (task.IsCanceled || task.IsFaulted)
-                {
-                    Debug.LogError("계정 삭제 실패: " + task.Exception);
-                    callback?.Invoke(false);
-                }
-                else
-                {
-                    Debug.Log("계정 삭제 성공");
-                    callback?.Invoke(true);
-                }
-            });
-    }
+    //public void DeleteAccount(Action<bool> callback = null)
+    //{
+    //    Auth.CurrentUser.DeleteAsync()
+    //        .ContinueWithOnMainThread(task =>
+    //        {
+    //            if (task.IsCanceled || task.IsFaulted)
+    //            {
+    //                Debug.LogError("계정 삭제 실패: " + task.Exception);
+    //                callback?.Invoke(false);
+    //            }
+    //            else
+    //            {
+    //                Debug.Log("계정 삭제 성공");
+    //                callback?.Invoke(true);
+    //            }
+    //        });
+    //}
 
     public void SignOut()
     {
